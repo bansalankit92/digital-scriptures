522f8753255a9cfa03d0028256f56a8bfef68461
Ankit Bansal <ankit.bansal@peerlist.io>
2024-02-23 22:29:01 +0530

    updated site title and og image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
             // activeBaseRegex: 'kabir-sagar',
           },
 
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           // {
           //   href: 'https://github.com/bansalankit92/digital-scriptures',
           //   label: 'GitHub',
